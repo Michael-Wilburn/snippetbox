d56caf5fed97f33e0ea4928103e077dcc3182e8d
Michael Wilburn <michael-mw@hotmail.com>
2024-09-16 20:12:50 -0300

    fix extension file

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -46,7 +46,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	// And do the same thing again here...
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
-	app.render(w, http.StatusOK, "view.tmpl", data)
+	app.render(w, http.StatusOK, "view.html", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
