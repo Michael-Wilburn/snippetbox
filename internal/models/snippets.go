122b5483e8aaa067291bbfebf70189f2e7a85fae
Michael Wilburn <michael-mw@hotmail.com>
2024-11-09 16:34:56 -0300

    Executing the query for Insert service

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -8,7 +8,7 @@ import (
 // Define a Snippet type to hold the data for an individual snippet. notice how
 // the fields of the struct correspond to the field in pur MySQL snippets table\
 type Snippet struct {
-	ID      uint
+	ID      int
 	Title   string
 	Content string
 	Created time.Time
@@ -20,17 +20,40 @@ type SnippetModel struct {
 	DB *sql.DB
 }
 
-// This will insert a new snippet into the database.
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-	return 0, nil
+	// Write the SQL statement we want to execute. I've split it over two lines
+	// for readability (which is why it's surrounded with backquotes insted
+	// of normal double quotes)
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+ 	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	// Use the Exec() method on the embedded conncetion pool to execute the
+	// statement. The first parameter is the SQL statement, followed by the
+	// title, content and expiry valuees for the placeholder parameters. This
+	// method returns a sql.Result type, which contains some basic
+	// information about what happened when the statement was executed.
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// Use the LastInsertId() method on the result to get the ID of out
+	// newly inserted record int he snippet table.
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// The ID returned has the type int64, so we convert it to an int type
+	// before returning.
+	return int(id), nil
+
 }
 
-// This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id uint) (*Snippet, error) {
 	return nil, nil
 }
 
-// This wil return the most 10 recently created snippets.
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
 	return nil, nil
 }
