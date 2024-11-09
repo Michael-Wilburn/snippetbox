08c407d2451eeb1c8bc0d94c241873bee205e998
Michael Wilburn <michael-mw@hotmail.com>
2024-11-09 15:20:03 -0300

    Creating a database connection pool

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	// New import
+
+	"crypto/tls"
+	"crypto/x509"
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -13,12 +21,26 @@ type application struct {
 }
 
 func main() {
-
 	addr := flag.String("addr", ":4000", "HTTP network addres")
+
+	// Define a new command-line flag for the MySQL DSN string.
+	dsn := flag.String("dsn", os.Getenv("SNIPPETBOX_DB_DSN"), "MySQL data source name")
+
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// To keep the main() function tidy I've put the code for creating a connection
+	// pool into the separate openDB() function below. We pass openDB() the DSN
+	// from the command-line flag.
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	// We also defer a call to db.Close(), so that the connection pool is closed
+	// before the main() function exits.
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
@@ -33,6 +55,33 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+// we create this function to save in the rootCertPool the CA vertificate
+func (app *application) init() {
+	rootCertPool := x509.NewCertPool()
+	pem, err := os.ReadFile("database/cert/ca.pem")
+	if err != nil {
+		app.errorLog.Fatalf("Failed to read CA certificate file at 'database/cert/ca.pem': %v", err)
+	}
+	if !rootCertPool.AppendCertsFromPEM(pem) {
+		app.errorLog.Fatal("Failed to add CA certificate to the certificate pool")
+	}
+
+	mysql.RegisterTLSConfig("custom", &tls.Config{
+		RootCAs: rootCertPool,
+	})
 }
